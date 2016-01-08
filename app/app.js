b5a8f2dac8f66f745a12c694b1bd99cc0da06bc3
Joshua Book <joshua.book@gmail.com>
2016-01-08 11:19:44 -0500

    add react-dom

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
-var React = require('react');
+var ReactDOM = require('react-dom');
 var Greeting = require('./greeting');
 
-React.render(
+ReactDOM.render(
   <Greetinge name="world"/>,
   document.body
 );
